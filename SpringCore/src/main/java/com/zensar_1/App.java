9618f420e1b28146526865ccb32387cba44abd70
raja <raja.m@zensar.com>
2022-04-03 01:06:29 +0530

    project renamed

--- SpringCore/src/main/java/com/zensar_1/App.java
+++ SpringCore/src/main/java/com/zensar_1/App.java
@@ -5,13 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		AbstractApplicationContext iocContainer = new ClassPathXmlApplicationContext("BeansClassChat.xml");
-		ChatApplication chatApplication = (ChatApplication)iocContainer.getBean(ChatApplication.class);
-		
-		System.out.println(chatApplication);
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	AbstractApplicationContext iocContainer = new ClassPathXmlApplicationContext("BeansClassChat.xml");
+	ChatApplication chatApplication = (ChatApplication) iocContainer.getBean(ChatApplication.class);
+	System.out.println(chatApplication);
 
-	}
+    }
 
 }
